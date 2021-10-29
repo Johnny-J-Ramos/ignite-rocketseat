b51d02cb90b297f7f191d51424c268047c9e85a9
Johnny Jefferson José Ramos <61524749+Johnny-J-Ramos@users.noreply.github.com>
2021-10-29 11:07:47 -0300

    alterações nos json

--- fundamentos-nodejs/src/index.js
+++ fundamentos-nodejs/src/index.js
@@ -1,18 +1,62 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
 
 /**
  * GET      => Buscar uma informação dentro do servidor
  * POST     => Inserir uma informação dentro no servidor
- * DELETE   =>
- * PACH     =>
+ * PUT      => Serve para alterar uma informação no servidor
+ * PATCH    => Alterar uma informação especifica
+ * DELETE   => Deletar uma informação no Servidor
  */
 
 // localhost:3333
-app.get("/", (request, response) => {
-   return response.json({ message: "Johnny Jefferson José Ramos"});
+//GET
+app.get("/courses", (request, response) => {
+   return response.json([
+      "Curso 01",
+      "Curso 02",
+      "Curso 03"
+   ])
 });
+//POST
+app.post("/courses", (request, response) => {
+   return response.json([
+      "Curso 01",
+      "Curso 02",
+      "Curso 03",
+      "Curso 04"
+   ])
+})
+//PUT
+app.put("/courses/:id", (request, response) => {
+   return response.json([
+      "Curso 6",
+      "Curso 02",
+      "Curso 07",
+      "Curso 04"
+   ])
+} )
+//PATCH 
+app.patch("/courses/:id", (require, response) => {
+   return response.json([
+      "Curso 01",
+      "Curso 02",
+      "Curso 08",
+      "Curso 04"
+   ])
+})
+//DELETE
+app.delete("/courses/:id", (require, response) => {
+   return response.json([ 
+      "Curso 01",
+      "Curso 02",
+      "Curso 04"
+
+   ])
+})
 
 //post
 app.listen(3333);
