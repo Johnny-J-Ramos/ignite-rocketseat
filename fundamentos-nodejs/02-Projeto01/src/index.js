61d30cd6b44a45cb81a82fade47f44c0205a18d5
Johnny Jefferson José Ramos <61524749+Johnny-J-Ramos@users.noreply.github.com>
2021-10-30 21:11:32 -0300

    incluir regra de negócio

--- fundamentos-nodejs/02-Projeto01/src/index.js
+++ fundamentos-nodejs/02-Projeto01/src/index.js
@@ -1,1 +1,56 @@
-const name = 'Johnny'
+const express = require("express");
+//id random
+const { v4: uuidv4 } = require("uuid") // biblioteca uuid - vai gerar um 
+const app = express();
+
+app.use(express.json());
+
+const customers = []; // array com dados ficticios 
+
+/**
+ * cpf - string
+ * name - string
+ * id - uuid Será necessário instalar essa biblioteca UUID - yarn add uuid )
+ * 
+ * statement - []
+ */
+ 
+//Register an account 
+app.post("/account", (request, response) => {
+   const { cpf, name } = request.body;
+
+   //CPF already exists
+   const customerAlreadyExists = customers.some((customer) => customer.cpf === cpf);
+ 
+   if(customerAlreadyExists) {
+      return response.status(400).json({ error: "CUstomer Already exists"});
+   }
+
+   const id = uuidv4();
+   //ID random
+
+   customers.push({
+      cpf,
+      name,
+      id: uuidv4(),
+      statement: [],
+   });
+
+   return response.status(201).send();
+
+})
+
+//Statement - demonstração
+app.get("/statement/:cpf", (request, response) => {
+   const {cpf} = request.params;
+
+   const customer = customers.find(customer => customer.cpf === cpf);
+
+   if(!customer) {
+      return response.status(400).json({error: "Customer not found"})
+   }
+
+   return response.status(200).json(customer.statement);
+})
+
+app.listen(3333);
